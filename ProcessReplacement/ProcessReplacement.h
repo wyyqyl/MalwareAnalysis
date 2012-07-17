48f8de0beef9efb63408e8cb2813013d53f54f7b
Yorath Wang <yorathwang@gmail.com>
2012-07-17 13:21:54 +0800

    First commit

---
+++ ProcessReplacement/ProcessReplacement.h
@@ -0,0 +1,20 @@
+#ifndef _PROCESS_REPLACEMENT
+#define _PROCESS_REPLACEMENT
+#include <Windows.h>
+
+typedef struct _PEB {
+  BYTE                          Reserved1[2];
+  BYTE                          BeingDebugged;
+  BYTE                          Reserved2[1];
+  PVOID                         Reserved3[2];
+  PPEB_LDR_DATA                 Ldr;
+  PRTL_USER_PROCESS_PARAMETERS  ProcessParameters;
+  BYTE                          Reserved4[104];
+  PVOID                         Reserved5[52];
+  PPS_POST_PROCESS_INIT_ROUTINE PostProcessInitRoutine;
+  BYTE                          Reserved6[128];
+  PVOID                         Reserved7[1];
+  ULONG                         SessionId;
+} PEB, *PPEB;
+
+#endif
